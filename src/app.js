2840d0c351b879eca6abed3c3dcdaf386fe3e408
K0ST4N <kostantinatanasov11@gmail.com>
2025-06-23 19:47:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+const opciones = ['piedra', 'papel', 'tijera', 'lizard', 'spock'];
 
+const pregunta = prompt("¡Elige una opción!: piedra, papel, tijera, lizard, spock").toLowerCase();
+
+// Genera una opción aleatoria para el bot
+const opcionAleatoria = () => {
+  const index = Math.floor(Math.random() * opciones.length);
+  return opciones[index];
+};
+
+const seleccionUsuario = pregunta;
+const seleccionBot = opcionAleatoria();
+
+console.log(`Seleccionaste: ${seleccionUsuario}`);
+console.log(`El bot seleccionó: ${seleccionBot}`);
+
+const juego = (usuario, bot) => {
+  if (usuario === bot) {
+    console.log("¡Es un empate!");
+  } else if (
+    (usuario === "piedra" && (bot === "tijera" || bot === "lizard")) ||
+    (usuario === "papel" && (bot === "piedra" || bot === "spock")) ||
+    (usuario === "tijera" && (bot === "papel" || bot === "lizard")) ||
+    (usuario === "lizard" && (bot === "papel" || bot === "spock")) ||
+    (usuario === "spock" && (bot === "tijera" || bot === "piedra"))
+  ) {
+    console.log("¡Ganaste!");
+  } else {
+    console.log("Has perdido, inténtalo de nuevo.");
+  }
+};
+
+juego(seleccionUsuario, seleccionBot);
